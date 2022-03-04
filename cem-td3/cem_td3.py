4e0a43af254079d41debaab04a8e7d7f3e05c7bc
Theo Charlot <62436507+labicquette@users.noreply.github.com>
2022-03-04 11:28:06 +0100

    added set_params

--- cem-td3/cem_td3.py
+++ cem-td3/cem_td3.py
@@ -88,6 +88,9 @@ def soft_update_params(net, target_net, tau):
     for param, target_param in zip(net.parameters(), target_net.parameters()):
         target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
 
+def set_params(net, target_net, tau):
+    for param, target_param in zip(net.parameters(), target_net.parameters()):
+        target_param.data.copy_(param.data)
 
 def _state_dict(agent, device):
     sd = agent.state_dict()
